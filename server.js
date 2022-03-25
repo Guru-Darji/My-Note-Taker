b126d74b34c392faef1b1b877f8d4c4c53dc0110
Guru Darji <gurudarji@ymail.com>
2022-03-25 13:07:13 -0400

    added delete function

--- server.js
+++ server.js
@@ -20,11 +20,67 @@ app.get("/notes", function (req, res) {
 })
 
 
+app.route("/api/notes")
+
+    .get(function (req, res) {
+        res.json(database);
+    })
+
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
 
 
+        let highestId = 99;
+
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+
+                highestId = individualNote.id;
+            }
+        }
+
+        newNote.id = highestId + 1;
+
+        database.push(newNote)
 
 
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
 
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+
+        res.json(newNote);
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+      let jsonFilePath = path.join(__dirname, "/db/db.json");
+      // request to delete note by id.
+      for (let i = 0; i < database.length; i++) {
+  
+          if (database[i].id == req.params.id) {
+              // Splice takes i position, and then deletes the 1 note.
+              database.splice(i, 1);
+              break;
+          }
+      }
+      // Write the db.json file again.
+      fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+  
+          if (err) {
+              return console.log(err);
+          } else {
+              console.log("Your note was deleted!");
+          }
+      });
+      res.json(database);
+  });
+  
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
